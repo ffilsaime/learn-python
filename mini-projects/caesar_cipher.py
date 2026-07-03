8e6d768dc7099e2fe9c2a8016159fff61113cef9
Florebencia <florebencia.filsaime1998@gmail.com>
2026-07-03 14:24:20 -0400

    Created a Caesar Cipher Game

---
+++ mini-projects/caesar_cipher.py
@@ -0,0 +1,34 @@
+import static.caesar_cipher_art
+
+logo = static.caesar_cipher_art.logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+        if letter in alphabet:
+
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
+    print(f"Here is the result: {output_text}")
+
+restart = True
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    response = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    restart = response == "yes"
+
+
+
+
